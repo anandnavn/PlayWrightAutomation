0f3c695f5359c7f1566eca03e7397ad2888376c5
Navneet Anand <1629076@kiit.ac.in>
2026-01-05 20:10:10 +0530

    Refactor code structure for improved readability and maintainability

--- tests/UIbasicTests.spec.js
+++ tests/UIbasicTests.spec.js
@@ -12,3 +12,17 @@ test('UI basic test', async ({browser}) =>
    //await page.screenshot({ path: 'example.png' });
    
 });
+
+test('Playwright test', async ({browser}) => 
+{
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const title = await page.title();
+    console.log(`Page title is: ${title}`);
+    await page.locator('#username').fill('Navneet');
+    await page.locator('#password').fill('Navneet@123');
+    await page.locator('#signInBtn').click();    
+    await page.screenshot({ path: 'loginpage.png' });
+});
